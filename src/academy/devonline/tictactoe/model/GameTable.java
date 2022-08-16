681405c94e7972227e762ba5db378b2ebed869f7
igorsergeevich888 <igorbolshakow@mail.ru>
2022-08-16 20:17:19 +0700

    Overide toString methods for model classes

--- src/academy/devonline/tictactoe/model/GameTable.java
+++ src/academy/devonline/tictactoe/model/GameTable.java
@@ -16,6 +16,8 @@
 
 package academy.devonline.tictactoe.model;
 
+import java.util.Arrays;
+
 /**
  * @author devonline
  * @link http://devonline.academy/java
@@ -40,5 +42,17 @@ public class GameTable {
         table[cell.getRow()][cell.getCol()] = sign;
     }
 
-
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("GameTable{");
+        sb.append("table=");
+        for (int i = 0; i < table.length; i++) {
+            sb.append(Arrays.toString(table[i]));
+            if (i < table.length - 1) {
+                sb.append(';');
+            }
+        }
+        sb.append('}');
+        return sb.toString();
+    }
 }
