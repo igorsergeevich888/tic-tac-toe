48c17511906719660d0bf234bcbb8857f8e264e6
igorsergeevich888 <igorbolshakow@mail.ru>
2022-10-29 21:17:05 +0700

    Add Sign enum

--- src/main/java/academy/devonline/tictactoe/model/Sign.java
+++ src/main/java/academy/devonline/tictactoe/model/Sign.java
@@ -14,31 +14,26 @@
  * limitations under the License.
  */
 
-package academy.devonline.tictactoe.component;
-
-import academy.devonline.tictactoe.model.Cell;
-import academy.devonline.tictactoe.model.GameTable;
-
-import java.util.Random;
-
-import static academy.devonline.tictactoe.model.Sign.O;
+package academy.devonline.tictactoe.model;
 
 /**
  * @author devonline
  * @link http://devonline.academy/java
  */
-public class ComputerMove {
-    public void make(final GameTable gameTable) {
+public enum Sign {
 
-        Random random = new Random();
-        while (true) {
-            final int row = random.nextInt(3);
-            final int col = random.nextInt(3);
-            final Cell randomCell = new Cell(row, col);
-            if (gameTable.isEmpty(randomCell)) {
-                gameTable.setSign(randomCell, O);
-                return;
-            }
+    X,
+
+    O,
+
+    EMPTY;
+
+    @Override
+    public String toString() {
+        if (this == EMPTY) {
+            return " ";
+        } else {
+            return name();
         }
     }
 }
