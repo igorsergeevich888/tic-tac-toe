684a9e7f08c1d9acc44fa0ddc38ad79066d9c0f8
igorsergeevich888 <igorbolshakow@mail.ru>
2022-11-04 16:35:38 +0700

    Add CommandLineArgumentParser component

--- src/main/java/academy/devonline/tictactoe/component/GameFactory.java
+++ src/main/java/academy/devonline/tictactoe/component/GameFactory.java
@@ -20,7 +20,6 @@ import academy.devonline.tictactoe.component.keypad.DesktopNumericKeypadCellNumb
 import academy.devonline.tictactoe.model.Player;
 import academy.devonline.tictactoe.model.PlayerType;
 
-import static academy.devonline.tictactoe.model.PlayerType.COMPUTER;
 import static academy.devonline.tictactoe.model.PlayerType.USER;
 import static academy.devonline.tictactoe.model.Sign.O;
 import static academy.devonline.tictactoe.model.Sign.X;
@@ -37,34 +36,12 @@ public class GameFactory {
 
     public GameFactory(final String[] args) {
 
-        PlayerType player1Type = null;
-        PlayerType player2Type = null;
-        for (final String arg : args) {
-            if (USER.name().equalsIgnoreCase(arg) || COMPUTER.name().equalsIgnoreCase(arg)) {
-                if (player1Type == null) {
-                    player1Type = PlayerType.valueOf(arg.toUpperCase());
-                } else if (player2Type == null) {
-                    player2Type = PlayerType.valueOf(arg.toUpperCase());
-                } else {
-                    System.err.println("Unsupported command line argument: '" + arg + "'");
-                }
 
-            } else {
-                System.err.println("Unsupported command line argument: '" + arg + "'");
-            }
-        }
+        final CommandLineArgumentParser.PlayerTypes playerTypes =
+                new CommandLineArgumentParser(args).parse();
+        this.player1Type = playerTypes.getPlayer1Type();
+        this.player2Type = playerTypes.getPlayer2Type();
 
-        if (player1Type == null) {
-            this.player1Type = USER;
-            this.player2Type = COMPUTER;
-        } else if (player2Type == null) {
-            this.player1Type = USER;
-            this.player2Type = player1Type;
-        } else {
-            this.player1Type = player1Type;
-            this.player2Type = player2Type;
-
-        }
 
     }
 
