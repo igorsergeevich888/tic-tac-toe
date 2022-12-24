4c995004ba9c02c72afdb12a6d0528f10c675796
igorsergeevich888 <igorbolshakow@mail.ru>
2022-12-24 11:46:39 +0700

    Fix toCell and toNumber for terminal impl

--- src/main/java/academy/devonline/tictactoe/component/console/keypad/TerminalNumericKeypadCellNumberConverter.java
+++ src/main/java/academy/devonline/tictactoe/component/console/keypad/TerminalNumericKeypadCellNumberConverter.java
@@ -19,6 +19,8 @@ package academy.devonline.tictactoe.component.console.keypad;
 import academy.devonline.tictactoe.component.console.CellNumberConverter;
 import academy.devonline.tictactoe.model.game.Cell;
 
+import static java.lang.String.format;
+
 /**
  * @author devonline
  * @link http://devonline.academy/java
@@ -33,20 +35,27 @@ public class TerminalNumericKeypadCellNumberConverter implements CellNumberConve
 
     @Override
     public Cell toCell(final char number) {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (mapping[i][j] == number) {
-                    return new Cell(i, j);
-                }
-            }
+        if (number >= '1' && number <= '9') {
+            final int val = number - '0' - 1;
+            return new Cell(val / 3, val % 3);
+        } else {
+            throw new IllegalArgumentException(
+                    format("Number parameter must be between '1' and '9'! Current value is '%s'!", number)
+            );
         }
-        return null;
     }
+
 
     @Override
     public char toNumber(final Cell cell) {
-        return mapping[cell.getRow()][cell.getCol()];
+        if (cell.getRow() >= 0 && cell.getRow() <= 2 && cell.getCol() >= 0 && cell.getCol() <= 2) {
+            return (char) ('0' + (cell.getRow() * 3 + cell.getCol() + 1));
+        } else {
+            throw new IllegalArgumentException(
+                    format("Row and col indexes must be between 0 and 2! Current row is %s, current col is %s!",
+                            cell.getRow(), cell.getCol()
+                    )
+            );
+        }
     }
-
-
 }
